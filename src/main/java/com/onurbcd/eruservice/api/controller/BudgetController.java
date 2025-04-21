f4da7f0e6dd7a097a9b88b5ef18c212fcaace264
Bruno Domingues <bruno.domingues@aignosi.com.br>
2025-04-20 23:50:51 -0300

    Remove BudgetController.

--- src/main/java/com/onurbcd/eruservice/api/controller/BudgetController.java
+++
@@ -1,9 +0,0 @@
-package com.onurbcd.eruservice.api.controller;
-
-/*
-    @DeleteMapping("/{year}/{month}/all")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    public void deleteAll(@PathVariable("year") Short refYear, @PathVariable("month") Short refMonth) {
-        budgetService.deleteAll(refYear, refMonth);
-    }
-}*/
